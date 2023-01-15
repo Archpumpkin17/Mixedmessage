8f0d24be2117980c9dcbe8381e2de09047bae503
Archpumpkin17 <archmaster140798@gmail.com>
2023-01-15 14:19:03 +0000

    Message generator

---
+++ mixedmessage.js
@@ -0,0 +1,43 @@
+
+function generateRandomNumber(num) {
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+  }
+  
+  const funny = {
+    words: ['Archie', 'Louisa', 'Pumpkin'],
+    words2:['kicked a', 'punched a', 'slapped a'],
+    words3:['dog', 'cat', 'poodle']
+  }
+  
+  // Store the 'wisdom' in an array
+  let funnysentence = []
+  
+  // Iterate over the object
+  for(let prop in funny) {
+    let optionIdx = generateRandomNumber(funny[prop].length)
+  
+    // use the object's properties to customize the message being added to personalWisdom  
+    switch(prop) {
+      case 'words':
+        funnysentence.push(`Today I saw that "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      case 'words2':
+        funnysentence.push({collectiveWisdom[prop][optionIdx]})
+        break
+      case 'words3':
+        funnysentence.push(`, How horrid "${collectiveWisdom[prop][optionIdx]}".`)
+        break
+      default:
+        personalWisdom.push("I didn't see anything.")
+    }
+  }
+  
+  function formatHorrid(wisdom) {
+    // Add some ASCII here?
+    // Add in more symbols to the array? like emojies or what not?
+    const formatted = funnysentence.join('\n')
+    console.log(formatted)
+  }
+  
+  formatWisdom(funnysentence);
